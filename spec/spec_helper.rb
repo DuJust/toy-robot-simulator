9ddf6b529895326fe39cd2b7239851be64f942a4
DuJust <du.just.it@gmail.com>
2016-10-19 22:43:36 +0800

    redirect stderr and stdout because some test output exception.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,3 +8,17 @@ RSpec.configure do |config|
   config.tty       = true
   config.formatter = :documentation
 end
+
+# Redirect stderr and stdout
+RSpec.configure do |config|
+  original_stderr = $stderr
+  original_stdout = $stdout
+  config.before(:all) do
+    $stderr = File.open(File::NULL, "w")
+    $stdout = File.open(File::NULL, "w")
+  end
+  config.after(:all) do
+    $stderr = original_stderr
+    $stdout = original_stdout
+  end
+end
