15ae3927328ddd8eaf06a414d68b4d3176c9306e
DuJust <du.just.it@gmail.com>
2016-10-19 18:05:09 +0800

    move robot forward north.

--- lib/toy_robot_simulator/robot.rb
+++ lib/toy_robot_simulator/robot.rb
@@ -1,13 +1,21 @@
 module ToyRobotSimulator
   class Robot
+    NORTH = 'NORTH'
+
     def place(x, y, orientation)
       @x           = x
       @y           = y
       @orientation = orientation
     end
-    
+
     def report
       [@x, @y, @orientation].join(',')
+    end
+
+    def move
+      if @orientation == NORTH
+        @y += 1
+      end
     end
   end
 end
--- spec/toy_robot_simulator/robot_spec.rb
+++ spec/toy_robot_simulator/robot_spec.rb
@@ -3,13 +3,22 @@ require 'toy_robot_simulator/robot'
 
 module ToyRobotSimulator
   describe Robot do
+    let(:robot) { Robot.new }
+
     describe '#place and #report' do
-      let(:robot) { Robot.new }
-      
       it 'should place robot to specific position' do
         robot.place(0, 0, 'NORTH')
-        
+
         expect(robot.report).to eq('0,0,NORTH')
+      end
+    end
+
+    describe '#move' do
+      it 'should move forward north' do
+        robot.place(0, 0, 'NORTH')
+        robot.move
+
+        expect(robot.report).to eq('0,1,NORTH')
       end
     end
   end
