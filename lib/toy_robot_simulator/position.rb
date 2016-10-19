a843942d3b66d1a83ed23a18dbb596b765298861
DuJust <du.just.it@gmail.com>
2016-10-20 01:24:03 +0800

    refactor position by validation with errors.

--- lib/toy_robot_simulator/position.rb
+++ lib/toy_robot_simulator/position.rb
@@ -13,6 +13,7 @@ module ToyRobotSimulator
     MAX_POSITION = 5
 
     attr_accessor :x, :y, :orientation
+    attr_reader :errors
 
     def initialize(x, y, orientation)
       @x           = x
@@ -21,10 +22,11 @@ module ToyRobotSimulator
     end
 
     def on_table?
-      x && y && orientation &&
-        x >= MIN_POSITION && x <= MAX_POSITION &&
-        y >= MIN_POSITION && y <= MAX_POSITION &&
-        ORIENTATIONS.include?(orientation)
+      @errors = []
+      validate_x
+      validate_y
+      validate_orientation
+      @errors.empty?
     end
 
     ORIENTATIONS.each do |orientation|
@@ -34,6 +36,36 @@ module ToyRobotSimulator
 
       define_method "turn_#{orientation.downcase}" do
         @orientation = orientation
+      end
+    end
+
+    private
+
+    def validate_x
+      unless x
+        @errors << 'Position X is empty.'
+      else
+        @errors << "Position X #{x} is less than 0." unless x >= MIN_POSITION
+        @errors << "Position X #{x} is more than 0." unless x <= MAX_POSITION
+      end
+    end
+
+    def validate_y
+      unless y
+        @errors << 'Position Y is empty.'
+      else
+        @errors << "Position Y #{y} is less than 0." unless y >= MIN_POSITION
+        @errors << "Position Y #{y} is more than 0." unless y <= MAX_POSITION
+      end
+    end
+
+    def validate_orientation
+      unless orientation
+        @errors << 'Position orientation is empty.'
+      else
+        unless ORIENTATIONS.include?(orientation)
+          @errors << "Position orientation #{orientation} is should be one of #{ORIENTATIONS.join(',')}."
+        end
       end
     end
   end
--- spec/toy_robot_simulator/position_spec.rb
+++ spec/toy_robot_simulator/position_spec.rb
@@ -18,18 +18,30 @@ module ToyRobotSimulator
           let(:x) { nil }
 
           it { is_expected.to be_falsey }
+          it 'should have position x empty error' do
+            subject
+            expect(position.errors).to include('Position X is empty.')
+          end
         end
 
         context 'when less than 0' do
           let(:x) { -1 }
 
           it { is_expected.to be_falsey }
+          it 'should have position x less than error' do
+            subject
+            expect(position.errors).to include('Position X -1 is less than 0.')
+          end
         end
 
         context 'when more than 5' do
           let(:x) { 6 }
 
           it { is_expected.to be_falsey }
+          it 'should have position x more than error' do
+            subject
+            expect(position.errors).to include('Position X 6 is more than 0.')
+          end
         end
       end
 
@@ -38,25 +50,53 @@ module ToyRobotSimulator
           let(:y) { nil }
 
           it { is_expected.to be_falsey }
+          it 'should have position y empty error' do
+            subject
+            expect(position.errors).to include('Position Y is empty.')
+          end
         end
 
         context 'when less than 0' do
           let(:y) { -1 }
 
           it { is_expected.to be_falsey }
+          it 'should have position y less than error' do
+            subject
+            expect(position.errors).to include('Position Y -1 is less than 0.')
+          end
         end
 
         context 'when more than 5' do
           let(:y) { 6 }
 
           it { is_expected.to be_falsey }
+          it 'should have position y more than error' do
+            subject
+            expect(position.errors).to include('Position Y 6 is more than 0.')
+          end
         end
       end
 
       context 'orientation' do
-        let(:orientation) { 'WHATEVER' }
+        context 'when nil' do
+          let(:orientation) { nil }
 
-        it { is_expected.to be_falsey }
+          it { is_expected.to be_falsey }
+          it 'should have position orientation empty error' do
+            subject
+            expect(position.errors).to include('Position orientation is empty.')
+          end
+        end
+
+        context 'when not orientation' do
+          let(:orientation) { 'WHATEVER' }
+
+          it { is_expected.to be_falsey }
+          it 'should have position orientation invalid error' do
+            subject
+            expect(position.errors).to include("Position orientation WHATEVER is should be one of NORTH,EAST,SOUTH,WEST.")
+          end
+        end
       end
     end
 
